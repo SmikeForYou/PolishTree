42541cf4b8ae65d8f5f7795aabc6dbbe5d6cac67
RicCucumber <rikcucumber@gmail.com>
2019-07-31 11:49:30 +0300

    фикс random_expression();
    добавлены отрицательные числа;

--- random_expression.py
+++ random_expression.py
@@ -1,7 +1,7 @@
 # /*функция-генератор случайных алгебраических выражений
 import random
 
-def random_expression():
+def random_expression(expression_len):
 
     def swithcer():
         """
@@ -15,51 +15,63 @@ def random_expression():
             else:
                 yield ''
 
-    def gen_atom_expression():
+    def gen_atom_expression(expression_len):
         """
         генератор случайного минимального алгебраического выражения
         вида значение - оператор - значение
         """
         #генерация листа случайной длины
         #для создания случайного кол-ва атомарных выражений
-        rnd_lst = [''] * random.randint(20, 45)
+        rnd_lst = [''] * expression_len
         bracket = swithcer()
         while len(rnd_lst) != 0:
             operator = random.choice(OPERATORS)
             #если оператор - логарифм или степень
             #ограничение диапазона случайных чисел (избежать больших значений)
-            if operator == 'log' or operator == '^':
+            if operator == 'log':
                 value1 = round(random.uniform(2, 10), 2)
                 value2 = round(random.uniform(0, 10), 2)
-                if operator == 'log':
-                    #если второй оператор логарифма меньши либо равен 1
-                    #берем логарифм по первому оператору (натуральный логарифм)
-                    if value2 <= 1:
-                        yield f'{operator}({value1})'
-                    else:
-                    #иначе берем логарифм по второму значению
-                        yield f'{operator}({value1},{value2})'
-                if operator == '^':
-                    yield f'{value1} {operator} {value2}'
-            else:
-                value1 = round(random.uniform(1, 1000), 2)
-                value2 = round(random.uniform(1, 1000), 2)
+                #если второй оператор логарифма меньши либо равен 1
+                #берем логарифм по первому оператору (натуральный логарифм)
+                if value2 <= 1:
+                    yield f'{operator}({value1})'
+                else:
+                #иначе берем логарифм по второму значению
+                    yield f'{operator}({value1},{value2})'
 
-                #рандомная вставка скобки
-                s = next(bracket)
-                if s == '(':
-                    yield f'{value1} {operator} {s} {value2}'
-                elif s == ')':
-                    yield f'{value1} {s} {operator} {value2}'
+                continue
+
+            if operator == '^':
+                value1 = round(random.uniform(-10, 10), 2)
+                value2 = round(random.uniform(-10, 10), 2)
+            else:
+                value1 = round(random.uniform(-100, 1000), 2)
+                value2 = round(random.uniform(-100, 1000), 2)
+
+            #рандомная вставка скобки
+            s = next(bracket)
+            if s == '(':
+                yield f'{value1} {operator} {s} {value2}'
+            elif s == ')':
+                yield f'{value1} {s} {operator} {value2}'
 
             rnd_lst.pop()
 
 
     def remove_bracket(expression_string):
         expression_list = expression_string.split(' ')
+        #Если скобок нет - вернуть исходное выражение
+        if expression_list.count('(') == 0 and expression_list.count(')') == 0:
+            return ' '.join(expression_list)
+        #Если есть только открывающая скобка - удалить ее
+        elif expression_list.count(')') == 0:
+            expression_list.remove('(')
+            return ' '.join(expression_list)
+
         expression_list.reverse()
         s1 = expression_list.index('(')
         s2 = expression_list.index(')')
+        #Если последняя скобка открывающая - удалить ее
         if s1 < s2:
             expression_list.remove('(')
         expression_list.reverse()
@@ -77,7 +89,7 @@ def random_expression():
     ]
 
     #список атомарных выражений, полученных из генератора
-    data = list(gen_atom_expression())
+    data = list(gen_atom_expression(expression_len))
     expr_list = []
     #цикл добавления случайных операторов между атомарными выраженями
     for a in range(0, len(data)):
